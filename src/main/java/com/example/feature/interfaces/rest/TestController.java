19179ff8e363fc3208e7b749841956fe1b37ff74
Wang Yanqun <wangyanqun@oppo.com>
2019-05-21 15:24:13 +0800

    update

--- src/main/java/com/example/feature/interfaces/rest/TestController.java
+++ src/main/java/com/example/feature/interfaces/rest/TestController.java
@@ -28,6 +28,7 @@ public class TestController {
     public String hello(){
         System.out.println("idea新增了一个方法");
         System.out.println(" update ");
+        System.out.println("hehe");
         return "success";
     }
     
